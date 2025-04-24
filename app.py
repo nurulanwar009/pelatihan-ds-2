abe818b5a860e1dda3769c257de02ddc5cad51a5
nurulanwar009 <nurulanwar009@gmail.com>
2025-04-24 09:29:31 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlist as st
+import joblib
+import numpy as np
+
+# Memuuat model regresi linier yang sudah disimpan
+lin_reg_load = joblib.load('lin_reg_model.joblib')
+
+# judul aplikasi
+st.title("Prediksi Gaji Berdasarkan Lama Bekerja")
+
+# Input tahun pengalaman kerja
+years_experience = st.number.input("Masukan jumlah tahun bekerja:", min_value=0.0, step=0.1)
+
+# Prediksi gaji
+if st.button("Prediksi Gaji")
+	gaji = lin_reg_loaded.predict([[years_experience]])
+	st.write(f"Gaji seorang setelah bekerja selama {years_experience} tahun adalah ${gaji[0]:,.sf} 
