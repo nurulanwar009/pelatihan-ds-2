4043617ff9d4541ef598e799a6ebd4b89ec6ba0b
nurulanwar009 <nurulanwar009@gmail.com>
2025-04-24 09:34:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ lin_reg_load = joblib.load('lin_reg_model.joblib')
 st.title("Prediksi Gaji Berdasarkan Lama Bekerja")
 
 # Input tahun pengalaman kerja
-years_experience = st.number.input("Masukan jumlah tahun bekerja:", min_value=0.0, step=0.1)
+years_experience = st.number_input("Masukan jumlah tahun bekerja:", min_value=0.0, step=0.1)
 
 # Prediksi gaji
 if st.button("Prediksi Gaji"):
