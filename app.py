33b49689af560f90bd28f3aad8340eea468373ed
nurulanwar009 <nurulanwar009@gmail.com>
2025-04-24 09:48:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import joblib
 import numpy as np
 
 # Memuuat model regresi linier yang sudah disimpan
-lin_reg_load = joblib.load('lin_reg_model.joblib')
+lin_reg_loaded = joblib.load('lin_reg_model.joblib')
 
 # judul aplikasi
 st.title("Prediksi Gaji Berdasarkan Lama Bekerja")
@@ -13,5 +13,5 @@ years_experience = st.number_input("Masukan jumlah tahun bekerja:", min_value=0.
 
 # Prediksi gaji
 if st.button("Prediksi Gaji"):
-	gaji = lin_reg_model.joblib.predict([[years_experience]])
+	gaji = lin_reg_loaded.predict([[years_experience]])
 	st.write(f"Gaji seorang setelah bekerja selama {years_experience} tahun adalah ${gaji[0]:,.2f})") 
